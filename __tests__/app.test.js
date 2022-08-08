a1a53b49517d8c1f62c428f8ecaf8446dc60661e
kathrynzaro <zarokathryn@gmail.com>
2022-08-08 13:53:31 -0700

    adds insert user route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,28 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
