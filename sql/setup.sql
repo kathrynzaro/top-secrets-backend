fb7402a03b4e10076d9a1b7c4c2215abfe02c241
kathrynzaro <zarokathryn@gmail.com>
2022-08-09 12:57:13 -0700

    setting up secrets table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,3 +11,16 @@ CREATE TABLE users (
   email TEXT NOT NULL,
   password_hash TEXT NOT NULL
 );
+
+CREATE TABLE secrets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO secrets (title, description) VALUES
+('Birds', 'Birds are fake.'),
+('Horses', 'Horses are from outerspace.'),
+('Utmost v. upmost', 'Utmost doesn''t make any sense. Upmost is the correct word.')
