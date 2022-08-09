86c44a54369970f809d9ac932f974b7c78b478b7
kathrynzaro <zarokathryn@gmail.com>
2022-08-09 14:56:48 -0700

    adds post secret route

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -38,6 +38,21 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/api/v1/secrets');
     expect(res.status).toEqual(401);
   });
+
+  it('#POST should add a new secret', async () => {
+    const secret = {
+      title: 'The Trees Are Watching',
+      description: 'Trees are spies. Do not trust trees.'
+    };
+    const [agent] = await registerAndLogin();
+    const res = await agent.post('/api/v1/secrets').send(secret);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      title: expect.any(String),
+      description: expect.any(String),
+      created_at: expect.anything()
+    });
+  });
   
   afterAll(() => {
     pool.end();
